cb59b40762575e2ff6d419afb9a04d78433400e4
kestuqas <k.reimeris@gmail.com>
2019-11-25 18:22:27 +0200

    Automatic profile creation added

--- profile_page/users/views.py
+++ profile_page/users/views.py
@@ -17,6 +17,7 @@ def register(request):
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
 	return render(request, 'users/profile.html')
